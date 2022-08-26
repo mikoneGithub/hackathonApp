2456bc19d5db9de816a8a729af1c08771909390d
Nils Podewski <podewski@adesso-mobile.de>
2022-08-26 09:42:55 +0200

    main: added generated levels

--- app/src/main/java/de/ams/hackathon/Engine.kt
+++ app/src/main/java/de/ams/hackathon/Engine.kt
@@ -1,12 +1,16 @@
 package de.ams.hackathon
 
 import android.graphics.Point
-import kotlin.random.Random
 
+// walkable == 0bxxxxxxx1
 val fWall = 0
 val fEmpty = 1
 val fCoin = 3
+val fOrigin = 7
 val fDestination = 5
+
+val levels = longArrayOf(
+    33, 0x1ffffffff, 0x100111004, 0x1df7557d5, 0x110455455, 0x15fdd55d5, 0x150115111, 0x177f75d7f, 0x114004101, 0x155df7ffd, 0x144410505, 0x1f77df575, 0x110441111, 0x15f77df5d, 0x141141145, 0x17dd5f5d5, 0x115110511, 0x1d5775d77, 0x111544541, 0x17d55557f, 0x144455511, 0x177df5d55, 0x144004145, 0x15fdfdffd, 0x140104105, 0x177d5fd75, 0x104544141, 0x1dd575d75, 0x111104415, 0x177fd77d5, 0x105054455, 0x1dd755d55, 0x110111, 0x1ffffffff)
 
 class Engine {
 
@@ -15,10 +19,10 @@ class Engine {
     val south = 2
     val west = 3
 
-    val rows = 20
-    val columns = 20
+    val rows = 33
+    val columns = 33
 
-    val origin: Point = Point(1, 1)
+    val origin: Point = Point(0, 1)
     val destination: Point = Point(9, 9)
     var path: IntArray = intArrayOf()
     var step = 0
@@ -28,20 +32,25 @@ class Engine {
     var lastPosition = Point(origin.x, origin.y)
 
     fun reset() {
-        for (x in 0..rows - 1) {
-            for (y in 0..columns - 1) {
-                if ((x == 0) or (y == 0) or (x == columns - 1) or (y == rows - 1)) {
-                    world[x][y] = fWall
-                } else {
-                    world[x][y] = fEmpty
-                }
+        val level = 0
+
+        for (y in 0 until rows - 1) {
+            var row = levels[1 + (1 + rows) * level + y]
+
+            for (x in 0 until columns - 1) {
+                world[x][y] = if ((row % 2L) == 1L) fWall else fEmpty
+
+                row = row shr 1
             }
         }
+
+        world[0][1] = fOrigin
+        world[columns-1][rows-2] = fDestination
+
+        return
     }
 
     fun step() {
-        reset()
-
         print(step)
         print(path.size)
 
