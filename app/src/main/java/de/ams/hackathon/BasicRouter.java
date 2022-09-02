5bc35f54eb93baf734e9d8887fcd3a5cda177700
Nils Podewski <podewski@adesso-mobile.de>
2022-09-02 15:39:26 +0200

    main: base solution added as well as the missing images

--- app/src/main/java/de/ams/hackathon/BasicRouter.java
+++ app/src/main/java/de/ams/hackathon/BasicRouter.java
@@ -4,7 +4,11 @@ import android.graphics.Point;
 
 import org.jetbrains.annotations.NotNull;
 
-public class BasicRouter {
+import java.util.ArrayList;
+
+import kotlin.UIntArray;
+
+public class BasicRouter extends Router {
 
     public int[] solve(int width, int height, @NotNull int[][] world, @NotNull Point origin, @NotNull Point destination) {
         return new int[]{1, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 2, 2, 2, 2, 3, 3, 2, 2, 3, 3, 0, 0, 3, 3, 2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 1, 1, 0, 0, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1};
