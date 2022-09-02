6828c8083f94240d9fef53e041ede9f90f1ee4a0
Nils Podewski <podewski@adesso-mobile.de>
2022-09-02 21:41:10 +0200

    main: added missing constants

--- app/src/main/java/de/ams/hackathon/Router.java
+++ app/src/main/java/de/ams/hackathon/Router.java
@@ -11,6 +11,12 @@ public class Router {
     public static final int South = 2;
     public static final int West = 3;
 
+    public static final int fWall = 0;
+    public static final int fEmpty = 1;
+    public static final int fCoin = 3;
+    public static final int fDestination = 5;
+    public static final int fOrigin = 7;
+
     public int[] solve(int width, int height, @NotNull int[][] world, @NotNull Point origin, @NotNull Point destination) {
         return new int[0];
     }
